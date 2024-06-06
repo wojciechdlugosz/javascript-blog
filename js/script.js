5e8e0145254c5c76a2f58c616322ae78df7a6c88
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-06 14:51:09 +0200

    Remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -11,16 +11,27 @@ const titleClickHandler = function () {
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll(".titles a.active");
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
+
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll(".post.active");
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
 
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
-
 };
 
 const links = document.querySelectorAll(".titles a");
