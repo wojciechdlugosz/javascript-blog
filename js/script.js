b137ce82bac6a21b881cbb28a21bb47eb876a23e
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-13 15:25:23 +0200

    Add dynamic tags to article

--- js/script.js
+++ js/script.js
@@ -95,29 +95,43 @@ const titleClickHandler = function () {
 generateTitleLinks();
 
 function generateTags(){
+
   /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
 
   /* START LOOP: for every article: */
+  for (let article of articles) {
 
-  /* find tags wrapper */
+    /* find tags wrapper */
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
 
-  /* make html variable with empty string */
+    /* make html variable with empty string */
+    let html = '';
 
-  /* get tags from data-tags attribute */
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
 
-  /* split tags into array */
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
 
-  /* START LOOP: for each tag */
+    /* START LOOP: for each tag */
+    for(let tag of articleTagsArray){
 
-  /* generate HTML of the link */
+      /* generate HTML of the link */
+      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
 
-  /* add generated code to html variable */
+      /* add generated code to HTML variable */
+      html = html + linkHTML;
 
-  /* END LOOP: for each tag */
+    /* END LOOP: for each tag */
+    }
 
-  /* insert HTML of all the links into the tags wrapper */
+    /* insert HTML of all the links into the tags wrapper */
+    tagsWrapper.innerHTML = html;
 
   /* END LOOP: for every article: */
+  }
+
 }
 
 generateTags();
