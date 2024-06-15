c64c704538087c6d7c73126ef103955d10223653
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-15 15:41:04 +0200

    Add function calculateTagsParams

--- js/script.js
+++ js/script.js
@@ -95,6 +95,24 @@ generateTitleLinks();
 
 function calculateTagsParams(tags){
 
+  const params =
+  {
+    min: 999999,
+    max: 0
+  };
+
+  for(let tag in tags){
+    console.log(tag + ' is used ' + tags[tag] + ' times');
+    if(tags[tag] > params.max){
+      params.max = tags[tag];
+    }
+    if(tags[tag] < params.min){
+      params.min = tags[tag];
+    }
+  }
+
+  return params;
+
 }
 
 function generateTags(){
